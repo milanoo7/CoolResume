47bd92bfeb61dc49a65db388f57519ed8edb46c8
milanoo7 <pmh231294@gmail.com>
2021-04-28 21:58:57 -0400

    Updates

---
+++ precache-manifest.fabb83f8f3f5457f2ef88b545585081b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef74b0eaeec1434420c727bba03921c3",
+    "url": "/milanoo7.github.io/CoolResume/index.html"
+  },
+  {
+    "revision": "dc6096910b5e2dcfe306",
+    "url": "/milanoo7.github.io/CoolResume/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "33988bd250f6c6b33804",
+    "url": "/milanoo7.github.io/CoolResume/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "fcfb0cc44ef98c7da8a1",
+    "url": "/milanoo7.github.io/CoolResume/static/css/main.eb0ecea5.chunk.css"
+  },
+  {
+    "revision": "641a72d1ee9579bad2bd",
+    "url": "/milanoo7.github.io/CoolResume/static/js/2.af1e098e.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/milanoo7.github.io/CoolResume/static/js/2.af1e098e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc6096910b5e2dcfe306",
+    "url": "/milanoo7.github.io/CoolResume/static/js/3.0dc7d5d3.chunk.js"
+  },
+  {
+    "revision": "33988bd250f6c6b33804",
+    "url": "/milanoo7.github.io/CoolResume/static/js/4.3e0896ea.chunk.js"
+  },
+  {
+    "revision": "fcfb0cc44ef98c7da8a1",
+    "url": "/milanoo7.github.io/CoolResume/static/js/main.4a6e8cda.chunk.js"
+  },
+  {
+    "revision": "c9f14137989c30b09629",
+    "url": "/milanoo7.github.io/CoolResume/static/js/runtime-main.0966aaf9.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/milanoo7.github.io/CoolResume/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/milanoo7.github.io/CoolResume/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/milanoo7.github.io/CoolResume/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/milanoo7.github.io/CoolResume/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/milanoo7.github.io/CoolResume/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/milanoo7.github.io/CoolResume/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/milanoo7.github.io/CoolResume/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/milanoo7.github.io/CoolResume/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/milanoo7.github.io/CoolResume/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/milanoo7.github.io/CoolResume/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/milanoo7.github.io/CoolResume/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/milanoo7.github.io/CoolResume/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/milanoo7.github.io/CoolResume/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/milanoo7.github.io/CoolResume/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/milanoo7.github.io/CoolResume/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/milanoo7.github.io/CoolResume/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/milanoo7.github.io/CoolResume/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
